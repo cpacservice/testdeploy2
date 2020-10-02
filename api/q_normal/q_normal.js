90b06916d85f5537074c9733eff4e954b67813fe
nattharikanan <natthariknan@gmail.com>
2020-10-02 17:03:36 +0700

    email

--- api/q_normal/q_normal.js
+++ api/q_normal/q_normal.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-var nodemailer = require("nodemailer");
+var sendmail;
 module.exports = router;
+
+const nodemailer = require("nodemailer");
+// async..await is not allowed in global scope, must use a wrapper
 
 router.get("/", async (req, res) => {
   // ใช้ async function
@@ -58,81 +61,96 @@ router.post("/delete", async (req, res) => {
 });
 
 router.post("/insert", async (req, res) => {
-  var transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "natthariknan@gmail.com",
-      pass: "0873574010",
-    },
-  });
+  try {
+    let db = req.db;
+    let quanum;
+    let active = "กำลังดำเนินการ";
+    let ids = await db("q_normal")
+      .insert({
+        qNormalName: req.body.qNormalName,
+        qNormalUserid: req.body.qNormalUserid,
+        qNormalLast: req.body.qNormalLast,
+        qNormalPhone: req.body.qNormalPhone,
+        qNormalEmail: req.body.qNormalEmail,
+        qNormalInfo: req.body.qNormalNormalid,
+        qNormalAddressDelivery: req.body.qNormalAddressDelivery,
+        qNormalProductname: req.body.qNormalProductname,
+        qNormalProductid: req.body.qNormalProductid,
+        qNormalQuantity: req.body.qNormalQuantity,
+        qNormalUnittype: req.body.qNormalUnittype,
+        qNormalSquaremetre: req.body.qNormalSquaremetre,
+        qNormalDate: req.body.qNormalDate,
+        qNormalTime: req.body.qNormalTime,
+        qNormalStatus: active,
+      })
+      .returning("qNormalId")
+      .then(function (qNormalId) {
+        quanum = qNormalId;
+      });
+    async function sendMail() {
+      // สร้างออปเจ็ค transporter เพื่อกำหนดการเชื่อมต่อ SMTP และใช้ตอนส่งเมล
+      let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          // ข้อมูลการเข้าสู่ระบบ
+          user: "natthariknan@gmail.com", // email user ของเรา
+          pass: "0873574010", // email password
+        },
+      });
+      let infouser = await transporter.sendMail({
+        from: '"No reply 👻" <natthariknan@gmail.com>', // อีเมลผู้ส่ง
+        to: `${req.body.qNormalEmail}`, // อีเมลผู้รับ สามารถกำหนดได้มากกว่า 1 อีเมล โดยขั้นด้วย ,(Comma)
+        subject: "Hello ✔", // หัวข้ออีเมล
+        text: "ใบเสนอราคาของท่านกำลังดำเนินการ กรุณารอเจ้าหน้าที่ติดต่อกลับ", // plain text body
+        html: `<div class="text-center">
+        <h4 :style="{ paddingTop: '20px' }">
+          <b>บริษัท ผลิตภัณฑ์และวัตถุก่อสร้าง จำกัด</b>
+        </h4>
 
-  var mailOptions = {
-    from: "natthariknan@gmail.com",
-    to: "6006021630016@fitm.kmutnb.ac.th",
-    subject: "Sending Email using Node.js",
-    text: "That was easy!",
-  };
+        <div>
+          1516 ถ.ประชาราษฎร์ 1 แขวงวงศ์สว่าง เขตบางซื่อ กรุงเทพฯ 10800
+          <br />โทร.02-555-5000 CPAC CALL CENTER 02-555-5555 Email:
+          cpacinside@scg.com
+        </div>
+      </div>
+     `, // html body
+      });
+      console.log("Message sent: %s", infouser.messageId);
+    }
 
-  let db = req.db;
-  let quanum;
-  let active = "กำลังดำเนินการ";
-  let ids = await db("q_normal")
-    .insert({
-      qNormalName: req.body.qNormalName,
-      qNormalUserid: req.body.qNormalUserid,
-      qNormalLast: req.body.qNormalLast,
-      qNormalPhone: req.body.qNormalPhone,
-      qNormalEmail: req.body.qNormalEmail,
-      qNormalInfo: req.body.qNormalNormalid,
-      qNormalAddressDelivery: req.body.qNormalAddressDelivery,
-      qNormalProductname: req.body.qNormalProductname,
-      qNormalProductid: req.body.qNormalProductid,
-      qNormalQuantity: req.body.qNormalQuantity,
-      qNormalUnittype: req.body.qNormalUnittype,
-      qNormalSquaremetre: req.body.qNormalSquaremetre,
-      qNormalDate: req.body.qNormalDate,
-      qNormalTime: req.body.qNormalTime,
-      qNormalStatus: active,
-    })
-    .returning("qNormalId")
-    .then(function (qNormalId) {
-      quanum = qNormalId;
+    async function sendMailtoadmin() {
+      // สร้างออปเจ็ค transporter เพื่อกำหนดการเชื่อมต่อ SMTP และใช้ตอนส่งเมล
+      let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          // ข้อมูลการเข้าสู่ระบบ
+          user: "natthariknan@gmail.com", // email user ของเรา
+          pass: "0873574010", // email password
+        },
+      });
+      let infoadmin = await transporter.sendMail({
+        from: '"No reply 👻" <natthariknan@gmail.com>', // อีเมลผู้ส่ง
+        to: "natthariknan@gmail.com", // อีเมลผู้รับ สามารถกำหนดได้มากกว่า 1 อีเมล โดยขั้นด้วย ,(Comma)
+        subject: "Hello ✔", // หัวข้ออีเมล
+        text: "", // plain text body
+        html: "<b>มีรายการใบขอเสนอราคาจาก" + `${req.body.qNormalName}` + "</b>", // html body
+      });
+      console.log("Message sent: %s", infoadmin.messageId);
+    }
+    sendMail().catch(console.error);
+    sendMailtoadmin().catch(console.error);
+    res.send({
+      ok: true,
+      Normal: ids,
+      lastid: quanum,
     });
-  res.send({
-    ok: true,
-    Normal: ids,
-    lastid: quanum,
-  });
-  if (res.send.ok) {
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
-    });
+  } catch (e) {
+    res.send({ ok: false, error: e.message });
   }
 });
-//   router.get("/search", async (req, res) => {
-//     // ใช้ async function
-//     try {
-//       let db = req.db;
-//       let rows;
-//       if (req.query.productname) {
-//         // rows = await db("products")
-//         //   .where("productname", "like", '%' + req.query.productname + '%',"productid","like",'%' + req.query.productname + '%')
-//         rows = await db("products as p")
-//           .join("categories as c", "c.categoryid", "p.categoryid")
-//           .where("productname", "like", "%" + req.query.productname + "%")
-//           .orWhere("categoryname", "like", "%" + req.query.productname + "%");
-//       } else {
-//         rows = await db("products");
-//       }
-//       res.send({
-//         ok: true, // ส่ง status
-//         products: rows, // ส่งค่ากลับ
-//       });
-//     } catch (e) {
-//       res.send({ ok: false, error: e.message });
-//     }
-//   });
+
+//
